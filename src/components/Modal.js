2cb91f7fb5b5fae6cd3d22be713270f8c1347a74
AliciaKay <alicia.kaye1@gmail.com>
2021-08-28 15:04:23 -0500

    added view last message functionality

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -9,10 +9,12 @@ class Modal extends Component {
             company: props.company,
             template: props.template,
             wasSent: false,
-            selectedCustom: false
+            selectedCustom: false,
+            lastMessage: ''
         };
         this.setSentStatus = this.setSentStatus.bind(this);
         this.handleCustom = this.handleCustom.bind(this);
+        this.handleText = this.handleText.bind(this);
     }
 
     setSentStatus() {
@@ -20,15 +22,20 @@ class Modal extends Component {
     }
 
     handleCustom() {
-        this.setState({selectedCustom: true})
+        this.setState({selectedCustom: !this.state.selectedCustom})
+    }
+
+    handleText(e) {
+        this.setState({lastMessage: e.target.value});
     }
 
     writeCustom(guest, template, company) {
         let jsx = [];
         jsx.push(
             <div>
-            <textarea class="form-control" id="formControlTextarea" rows="3" defaultValue={template.greeting+" "+guest.firstName+template.welcome+" "+company+template.room+" "+template.signOff}></textarea>
+            <textarea class="form-control" id="formControlTextarea" name="messageContent" onChange={this.handleText} rows="3" defaultValue={template.greeting+" "+guest.firstName+template.welcome+" "+company+template.room+" "+guest.reservation.roomNumber+" "+template.signOff}></textarea>
             <p><em>Note: Messages over 140 characters may be split into multiple messages.</em></p>
+            <button type="button" className="btn btn-info" onClick={this.handleCustom}>Return to Standard</button>
             </div>
         );
         return jsx;
@@ -37,14 +44,16 @@ class Modal extends Component {
     makeStandard(guest, template, company) {
         let jsx = [];
         jsx.push(
+            <div>
             <p>{template.greeting} {guest.firstName}{template.welcome} {company}{template.room} {guest.reservation.roomNumber} {template.signOff}</p>
+            <button type="button" className="btn btn-info" onClick={this.handleCustom}>Create Custom Message?</button>
+            </div>
         );
         return jsx;
     }
 
 
 render() {
-    console.log(this.props.company);
     let guest = this.state.guest;
     let template = this.state.template;
     let company = this.props.company;
@@ -64,13 +73,13 @@ render() {
             <div className="modal-dialog">
                 <div className="modal-content">
                     <div className="modal-header">
-                        <h5 className="modal-title" id={"templateModalLabel"+guest.id}>Draft your message</h5>
+                        {this.state.wasSent ? <h5 className="modal-title" id={"templateModalLabel"+guest.id}>View last message</h5> : <h5 className="modal-title" id={"templateModalLabel"+guest.id}>Draft your message</h5>}
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                   {this.state.selectedCustom ? this.writeCustom(guest, template, company) : this.makeStandard(guest, template, company)}
-                    </div>
-                    <button type="button" className="btn btn-info" onClick={this.handleCustom}>Create Custom Message?</button>
+                        {this.state.wasSent && this.state.selectedCustom ? <div><p>{this.state.lastMessage}</p></div> : this.state.selectedCustom ? this.writeCustom(guest, template, company) : this.makeStandard(guest, template, company)}
+                        </div>
+                   
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="button" className="btn btn-primary"data-bs-dismiss="modal" onClick={this.setSentStatus}>Send Message</button>
